51d9394cf64d664128ebb7987df86b0d0bb44b7e
Kris Bulman <kris@bulman.ca>
2011-02-01 12:19:09 -0400

    fixed some designer bugs due to core changes. added search-block-form css id to searchtext.js

--- js/searchtext.js
+++ js/searchtext.js
@@ -3,6 +3,7 @@ if (Drupal.jsEnabled) {
  
     $('form#search-theme-form input.form-text').example('Search');
     $('form#search-box input.form-text').example('Search');
+    $('form#search-block-form input.form-text').example('Search');    
     $('form#islandora-solr-simple-search-form input.form-text').example('Search');    
     $('form#google-cse-searchbox-form input.form-text').example('Search');
   });
